0dda59322d82cfecb599d415755896c8d63316e8
pavlovaksenia <pavksan@gmail.com>
2022-01-30 23:10:21 +0700

    Создан проект

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите колличество элементов массива: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите элементы массива: ");
+string[] array = new string[number];
+for (int i = 0; i < number; i++)
+{
+    Console.Write(i + 1 + ". ");
+    array[i] = Console.ReadLine();
+}
+
+string[] newArray = new string[number];
+int j = 0;
+
+for (var i = 0; i < number; i++)
+{
+    if (array[i].Length < 4 && array[i].Length > 0)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+
+Array.Resize(ref newArray, j);
+
+if (newArray.Length > 0)
+{
+    Console.WriteLine("Новый массив: ");
+    for (var i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine(i + 1 + ". " + newArray[i]);
+    }
+}
+else
+{
+    Console.WriteLine("Новый массив пуст");
+}
